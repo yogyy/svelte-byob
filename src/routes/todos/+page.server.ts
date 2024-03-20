eea605ec3f0ef0e0ec361724bb2ea211c70dd94d
yogyy <yogimr261@gmail.com>
2024-03-20 18:46:45 +0700

    feat: add todos

---
+++ src/routes/todos/+page.server.ts
@@ -0,0 +1,51 @@
+import { getUserInfo, parseApiResponse } from '$lib/utils/index.js';
+import { error, redirect } from '@sveltejs/kit';
+
+export const load = async ({ locals }) => {
+  const { session, user } = await getUserInfo(locals);
+  if (!session || !user) {
+    redirect(307, '/');
+  }
+
+  const res = await parseApiResponse(locals.api.todos.$get());
+
+  if (res.error !== null) {
+    error(res.status, res.error);
+  }
+
+  return res.data;
+};
+
+export const actions = {
+  update: async ({ request, locals }) => {
+    const formData = await request.formData();
+
+    const todoId = formData.get('todoId');
+
+    if (!todoId) {
+      error(400, 'no todo id passed in...');
+    }
+
+    await locals.api.todos.complete.$post({
+      // again, really dumb way to do this, just showing off a proof of concept for now
+      form: { todoId: todoId.toString() }
+    });
+
+    // todo better error handling
+
+    return { success: true };
+  },
+  create: async ({ request, locals }) => {
+    const formData = await request.formData();
+
+    await locals.api.todos.$post({
+      form: {
+        name: formData.get('name') ?? ''
+      }
+    });
+
+    // todo better error handling
+
+    return { success: true };
+  }
+};
---
+++ src/routes/todos/+page.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  const { data } = $props();
+
+  console.log(data);
+</script>
+
+<svelte:head>
+  <title>svelte lucia hono 🔥/todos</title>
+</svelte:head>
+<div
+  class="container flex flex-col items-center justify-center text-white shadow-md"
+>
+  <h1 class="mb-6 text-3xl font-bold">Todo List</h1>
+  <ul class="space-y-4 pb-8">
+    {#each data.todos as todo}
+      <li class="flex items-center">
+        <form action="?/update" method="post">
+          <!-- this is done in a very silly way, but you get the idea of what it does, idc UI right now -->
+          <button class="flex flex-row items-center" type="submit">
+            <input type="text" name="todoId" class="hidden" value={todo.id} />
+            {#if todo.completed}
+              DONE
+            {:else}
+              NOT DONE
+            {/if}
+            {todo.name}
+          </button>
+        </form>
+      </li>
+    {/each}
+  </ul>
+
+  <form action="?/create" method="post">
+    <div>
+      <label for="name" class="block">name</label>
+      <input
+        type="text"
+        name="name"
+        id="name"
+        class="w-[400px] rounded-lg p-4 text-neutral-950"
+        placeholder="my sample todo"
+      />
+    </div>
+
+    <button
+      type="submit"
+      class="mt-2 rounded-md bg-neutral-800 p-1 font-semibold text-white"
+      >Create</button
+    >
+    <a
+      href="/auth/logout"
+      class="mt-2 rounded-md bg-neutral-800 p-1 font-semibold text-white"
+      >Logout</a
+    >
+  </form>
+</div>
